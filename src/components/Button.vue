d9f1b7dbff4238173a5e9f5b31fb2e49939af9a1
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2021-07-25 03:52:37 +0500

    ui done

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="$emit('btn-click')" :style="{background: color}" class="btn">{{text}}</button>    
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    emit: ['btn-click']
+    
+}
+</script>
+
+<style>
+    
+</style>
